236d42642383d37436f087318ed0a746d1ed9f8a
Leandro <souzadeoliveiraleandro@gmail.com>
2025-05-19 19:16:54 -0300

    atividade

--- api.js
+++ api.js
@@ -1,23 +1,43 @@
 const route = 'characters';
 var contador = 0;
-fetch(`https://dattebayo-api.onrender.com/${route}`, {
-    method: 'GET',
-    headers: {
+let minhaLista = [];
+fetch(`https://dattebayo-api.onrender.com/${route}`, {//chamdo de api
+    method: 'GET', // método para obter dados
+    headers: {//
         'Accept': 'application/json',
     },
 })
     .then((response) => {
-        return response.json();
+        return response.json();//
     })
     .then((data) => {
         while(contador < 18 ){
-            console.log(data.characters[contador].images[0]);
-            console.log(data.characters[contador].jutsu);
-            console.log(data.characters[contador].id[contador]);
+            //console.log(data.characters[contador].images[0]);
+            //console.log(data.characters[contador].jutsu);
+            minhaLista.push(data.characters[contador].id);
             contador ++;
             
         }
+
+        minhaLista.forEach((id) => {
+            console.log(id);
+            fetch(`https://dattebayo-api.onrender.com/${route}/${id}`,{
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                },
+            })    
+            .then((response) => {
+                return response.json();
+            }).then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })});
     })
     .catch((error) => {
         console.log(error);
-    })
+    });
+
+     
